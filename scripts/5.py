d80d421b1b5787d9057fae5a71b9cdce46443653
Vishal-Padia <vishalpadia9@gmail.com>
2022-03-15 13:12:33 +0530

    some basic scripts

---
+++ scripts/5.py
@@ -0,0 +1,16 @@
+#working with boolean types
+val1 = True
+print(val1)
+
+number = 10
+print(bool(number))
+
+number = -5
+print(bool(number))
+
+number = 0
+print(bool(number))
+
+val1 = 6
+val2 = 3
+print(val1 > val2)

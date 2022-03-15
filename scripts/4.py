71e2ebc909816b54dd46e5b2e6dc8bf5d156e4ed
Vishal-Padia <vishalpadia9@gmail.com>
2022-03-15 13:12:28 +0530

    some basic scripts

---
+++ scripts/4.py
@@ -0,0 +1,13 @@
+#rasing a number to the power
+import math 
+x = 4
+y = 3
+
+power = x ** y
+print("%d to the power %d is %d" % (x,y,power))
+
+power = pow(x,y)
+print("%d to the power %d is %d" % (x,y,power))
+
+power = math.pow(2,6.5)
+print("%d to the power %d is %d" % (x,y,power))

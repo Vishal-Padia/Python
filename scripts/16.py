87927545c15082aa2f458392a0be0d4acb02b764
Vishal-Padia <vishalpadia9@gmail.com>
2022-03-20 17:51:07 +0530

    added a script

---
+++ scripts/16.py
@@ -0,0 +1,13 @@
+#Add and Remove the item from a list
+
+fruits = ["Apple", "Mango", "Banana", "Guava"]
+
+fruits.insert(1, "watermelon")
+
+print("The Fruit list after inserting is: ")
+print(fruits)
+
+fruits.remove("Guava")
+
+print("The Fruit list after deleting: ")
+print(fruits)
